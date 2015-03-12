eeb4f5a0b619c21624aacf148d5577f7f4187c90
Manrique López <jsmanrique@gmail.com>
2015-03-12 19:19:20 +0100

    Added some data. Still work pending on directives

--- src/ng-vizgrimoire/resources/js/directives.js
+++ src/ng-vizgrimoire/resources/js/directives.js
@@ -1,27 +1,32 @@
 var vizgrimoireDirectives = angular.module('vizgrimoireDirectives', []);
 
-vizgrimoireDirectives.directive('vgsidemenu', function(){
+vizgrimoireDirectives.directive('vgsidemenu', function () {
     return {
         restrict: 'E',
         templateUrl: 'resources/templates/vgsidemenu.html',
         scope: {},
-        controller: function($scope, $http, $location, $routeParams) {
-            $http.get('resources/data/config.json').success(function(data){
+        controller: function ($scope, $http, $location, $routeParams) {
+            $http.get('resources/data/config.json').success(function (data) {
                 $scope.config = data;
             });
         }
     };
 });
 
-vizgrimoireDirectives.directive('vgwidget', function(){
+vizgrimoireDirectives.directive('vgwidget', function () {
     return {
         restrict: 'E',
         templateUrl: 'resources/templates/vgwidget.html',
-        scope: {},
-        controller: function($scope, $location, $routeParams) {
-            chartId = "chart" + Math.floor((Math.random()*1000)+1);
-            $scope.vgwidget = {title: $location.$$path, id: chartId, help: 'Lorem ipsum ad his scripta blandit partiendo'};
-            
+        scope: {
+            widget: '=type'
+        },
+        controller: function ($scope, $location, $routeParams) {
+            //var chartId = "chart" + Math.floor((Math.random() * 1000) + 1);
+            //$scope.widget = {title: 'Contributors', help: 'Contributors distribution'};
+            $scope.onion = {title: 'Contributors', help: 'Contributors distribution'};
+            $scope.activityagg = {title: 'Activity', help: 'Aggregated activity'};
+            $scope.activityts = {title: 'Activity', help: 'Activity over the time'};
+            //$scope.vgwidget = {title: $location.$$path, id: chartId, help: 'Lorem ipsum ad his scripta blandit partiendo'};
         }
     };
 });
