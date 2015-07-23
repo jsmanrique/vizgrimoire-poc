34f5078ec5b3ad31a527bf18330348ffecc66c48
Jose Manrique Lopez de la Fuente <jsmanrique@gmail.com>
2015-07-23 14:48:07 +0200

    removed TODO for jquery controlled sharing buttons

--- src/ng-vizgrimoire-2/app/scripts/main.js
+++ src/ng-vizgrimoire-2/app/scripts/main.js
@@ -2,20 +2,6 @@
 (function () {
 'use strict';
 
-// TODO: Do it with angular instead of jquery
-/*
-$(document).ready(function(){
-
-  $('#shareOnTW').click(function(){
-    window.location.href = 'https://twitter.com/share?url='+encodeURIComponent(document.URL)+'&text='+document.title+'&via=bitergia&hashtags=metrics,development';
-  });
-
-  $('#shareUrl').click(function(){
-    window.prompt('Copy to clipboard: CTRL+C / CMD+C, Enter', document.URL);
-  });
-});
-*/
-
 var app = angular.module('ngVizGrimoire', ['ngRoute', 'ui.bootstrap', 'nvd3ChartDirectives', 'vizgrimoireControllers', 'vizgrimoireDirectives']);
 
 app.config(['$routeProvider', function($routeProvider){
